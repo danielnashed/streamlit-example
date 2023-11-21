888d13a6c94700f563bfae3fe430cb62179a6d23
Daniel Nashed <daniel13sr@hotmail.com>
2023-11-21 02:45:23 +0000

    rev2

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,21 @@ import pandas as pd
 import streamlit as st
 from sqlalchemy import inspect
 
+st.set_page_config(
+    layout="wide")
+
+custom_css = """
+<style>
+h1 {
+    margin-top: 0;
+}
+</style>
+"""
+
+st.markdown(custom_css, unsafe_allow_html=True)
+
 def main():    
+    st.header('Road to 10,000 hours of AI', divider='gray')
     conn = st.connection('10000_db', type='sql')
     # Insert some data with conn.session.
     with conn.session as s:
@@ -19,8 +33,14 @@ def main():
         else:
             # query table to get stored hours 
             if 'hours' not in st.session_state:
-                result = conn.query('select * from hours_table')
-                st.session_state.hours = result['hours'].iloc[0]
+                # Execute SQL command to retrieve the maximum integer value from the 'hours' column
+                query_result = s.execute('SELECT MAX(hours) FROM hours_table WHERE hours IS NOT NULL;')
+                # Extract the result and handle potential errors
+                try:
+                    max_hours = int(query_result.scalar())
+                    st.session_state.hours = max_hours
+                except (TypeError, ValueError):
+                    st.session_state.hours = 0
         # update input field and output field 
         col1, col2 = st.columns([8, 1])
         with col1:
@@ -39,7 +59,6 @@ def main():
                 params={'cum_hours': st.session_state.hours}
             )
         s.commit()
-    st.header('Road to 10,000 hours of AI', divider='gray')
     
     # Create a 2D grid of points
     grid = []
@@ -76,19 +95,23 @@ def main():
     phantom = pd.DataFrame(flat_grid, columns=['x', 'y'])
     real = pd.DataFrame(flat_data, columns=['x', 'y'])
     
-    # Chart using Altair
-    chart = alt.Chart(pd.concat([phantom.assign(dataset='df1'), real.assign(dataset='df2')]), height=1000, width=2000).mark_circle(opacity=0.5).encode(
-    x=alt.X('x:Q', axis=alt.Axis(labels=False, ticks=False)),  # Hide x-axis labels and ticks
-    y=alt.Y('y:Q', axis=alt.Axis(labels=False, ticks=False)),  # Hide y-axis labels and ticks
-    color=alt.Color('dataset:N', scale=alt.Scale(range=['#0068c9', '#ff7f0e']))).configure_axis(
-    grid=False  # Hide gridlines  # Specify colors for each dataset
-).configure_legend(
-    title=None,  # Hide legend title
-    labelFontSize=0  # Hide legend labels
+    container = st.container()
+    with container:
+        # Chart using Altair
+        chart = alt.Chart(pd.concat([phantom.assign(dataset='df1'), real.assign(dataset='df2')]), height=1000, width=2000).mark_circle(size=20, opacity=0.5).encode(
+        x=alt.X('x:Q', axis=alt.Axis(labels=False, ticks=False, titleOpacity=0 )),  # Hide x-axis labels and ticks
+        y=alt.Y('y:Q', axis=alt.Axis(labels=False, ticks=False, titleOpacity=0)),  # Hide y-axis labels and ticks
+        color=alt.Color('dataset:N', scale=alt.Scale(range=['#0068c9', '#ff7f0e']))).configure_axis(
+        grid=False  # Hide gridlines  # Specify colors for each dataset
+    ).configure_legend(
+        title=None,  # Hide legend title
+        labelFontSize=0  # Hide legend labels
+    ).properties(
+    width=1500,  # Set width to 'container' to fit the chart into the Streamlit container
+    height=750  # Set height to 'container' to fit the chart into the Streamlit container
 )
-
-    # Display the Altair chart using Streamlit
-    st.altair_chart(chart)  
+        # Display the Altair chart using Streamlit
+        st.altair_chart(chart)  
  
 if __name__ == "__main__":
     main() 
